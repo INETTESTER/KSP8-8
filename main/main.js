f27b0f19245e1104a7e77fd06cdd85eaffbb8760
Tester Inet Doc <tester.inet.doc@gmail.com>
2026-07-15 11:11:51 +0700

    test

--- main/main.js
+++ main/main.js
@@ -20,8 +20,8 @@ import { InquiryTxn } from '../api/InquiryTxn.js';
 
 export default function () {    //เรียกใช้ API ใน export default function
   /// TTB
-  response = Authen()
-  //response = Authen_CreateTxn(cid)
+  //response = Authen()
+  response = Authen_CreateTxn(cid)
   //response = Authen_CreateTxn_CreateQR_TTB(cid)
 
   /// SCB
