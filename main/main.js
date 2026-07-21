414c31a8aac9909bb2ff16c624070dcf9cf3206c
Tester Inet Doc <tester.inet.doc@gmail.com>
2026-07-21 14:58:18 +0700

    test

--- main/main.js
+++ main/main.js
@@ -26,10 +26,10 @@ export default function () {    //เรียกใช้ API ใน export def
   //response = Authen_CreateTxn(cid)
   //response = Authen_CreateTxn_CreateQR_TTB(cid)
   //response = TTB_callback(scenario)
-  response = Authen_CreateTxn_CreateQR_Callback_TTB(cid, scenario)
+  //response = Authen_CreateTxn_CreateQR_Callback_TTB(cid, scenario)
 
   /// SCB
-  //response = Authen_CreateTxn_CreateQR_SCB(scenario)
+  response = Authen_CreateTxn_CreateQR_SCB(scenario)
   //response = SCB_callback()
   //response = Authen_CreateTxn_CreateQR_Callback_SCB()
 
